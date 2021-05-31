d0e2c05c31990c63c24679580959b298124dbf5b
alexisdjgoyo <alexisdjesusgoyoflores@gmail.com>
2021-05-31 07:41:25 -0400

    meta description

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -5,7 +5,9 @@
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <title>Showcase - Personal Portfolio One Page Bulma Theme</title>
+    <meta name="description" content="Hola, mi nombre es Alexis Goyo, Ingeniero en Informática y desarrollador web, bienvenido a mi sitio web donde te hablaré un poco sobre mi experiencia." />
+
+    <title>Alexis Goyo</title>
     <!-- Bulma Version 0.9.0-->
     <link rel="stylesheet" href="{{asset('/assets/css/bulma.css')}}" />
     <link rel="stylesheet" type="text/css" href="{{asset('/assets/css/custom.css')}}" />
