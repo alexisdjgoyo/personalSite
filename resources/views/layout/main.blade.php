78dbf8d40708d07d8a8a1c93cc65ad3c0acf92cf
alexisdjgoyo <alexisdjesusgoyoflores@gmail.com>
2022-04-10 18:41:57 -0400

    eliminado gtm

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -2,18 +2,6 @@
 <html lang="es">
 
 <head>
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-PBJQJ681VP"></script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
-
-        function gtag() {
-            dataLayer.push(arguments);
-        }
-        gtag('js', new Date());
-
-        gtag('config', 'G-PBJQJ681VP');
-    </script>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -190,7 +178,7 @@
     <!-- End Footer -->
 
     <!-- Scripts -->
-    <script src="{{ mix('js/app.js') }}"></script>
+    <script async src="{{ mix('js/app.js') }}"></script>
     {!! NoCaptcha::renderJs('es') !!}
     @stack('js')
 </body>
